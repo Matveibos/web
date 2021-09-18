1db62348fcb153dad2bd2d22a5528ade405d6a44
Matvei <matveibos@mail.ru>
2021-09-18 15:41:12 +0300

    final 2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,18 +5,40 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post=Post.new
   end
 
   def show
     @post=Post.find(params[:id])
   end
 
-  def create
-    @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+  def destroy
+    @post=Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
+  def edit
+    @post=Post.find(params[:id])
+  end
+
+  def update
+    @post=Post.find(params[:id])
+    if(@post.update(post_params))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if(@post.save)
+    redirect_to @post
+    else
+      render 'new'
+  end
+    end
 
   private def post_params
     params.require(:post).permit(:title,:body)
