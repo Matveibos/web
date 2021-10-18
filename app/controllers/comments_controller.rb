e3d675e446c5030e20335025639a5a451e5eef72
matveibos <matveibos@mail.ru>
2021-10-18 13:08:58 +0300

    10

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,21 @@
 class CommentsController < ApplicationController
   # http_basic_authenticate_with name:"admin",password: "123456"
 
+  # def new
+  #   @comment=Comment.new
+  # end
+
   def create
     @post = Post.find(params[:post_id])
     @comment= @post.comments.create(comment_params)
+
+    redirect_to post_path(@post)
+  end
+
+  def delete
+    @post = Post.find(params[:post_id])
+    @comment= @post.comments.delete(comment_params)
+
     redirect_to post_path(@post)
   end
 
