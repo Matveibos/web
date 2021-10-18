6afbffb3d60545ad8b77fe143a230204ade353c1
matveibos <matveibos@mail.ru>
2021-10-18 11:44:22 +0300

    10

---
+++ app/models/page.rb
@@ -0,0 +1,9 @@
+class Page < ApplicationRecord
+  has_many :comments
+  # has_many :images, service: :azure
+  validates :title, presence: true, length: {minimum: 4}
+  has_rich_text :body
+  belongs_to :user
+
+end
+
